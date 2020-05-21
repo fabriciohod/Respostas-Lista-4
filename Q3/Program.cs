6ccb3d0928dcf8e7d5aa0a622032ccc2e78b23ee
Fabricio Henrique De Oliveira Duarte <fabriciohod@hotmail.com>
2020-05-21 11:59:10 -0300

    Update Program.cs

--- Q3/Program.cs
+++ Q3/Program.cs
@@ -4,21 +4,18 @@ namespace Q3
 {
     public class Program
     {
-        public string ContagemDeNumerosRepitidos (int[] array, int comparador)
+         public static string ContagemDeNumerosRepitidos (int[] array, int comparador)
         {
-            int numero = 0;
             int qtdDeRepetissoes = 0;
             foreach (var item in array)
             {
                 if (item == comparador && array.GroupBy (x => x).Any (g => g.Count () > 1))
                 {
-                    numero = item;
                     qtdDeRepetissoes++;
                 }
             }
-            if (numero == 0) return "0";
-            return $"O numero {numero} se repete {qtdDeRepetissoes}";
+            if (qtdDeRepetissoes == 0) return "0";
+            return $"O numero {comparador} se repete {qtdDeRepetissoes}";
         }
-
     }
 }
